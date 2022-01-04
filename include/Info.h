69c3dec77d445bfcda97cb3a1b92176ce24ce19d
Asier Nuñez <45818936+Asiern@users.noreply.github.com>
2022-01-04 12:26:11 +0100

    Create Info.h

---
+++ include/Info.h
@@ -0,0 +1,17 @@
+/**
+ * @file Info.h
+ * @author Asiern
+ * @brief 
+ * @version 0.1
+ * @date 2022-01-03
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include <string>
+
+typedef struct
+{
+    std::string version;
+} info;
