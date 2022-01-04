fd57830d4d1f2c9bff9d120ab88b34b7b72e651a
Asier Nuñez <45818936+Asiern@users.noreply.github.com>
2022-01-04 12:26:09 +0100

    Create Offsets.h

---
+++ include/Offsets.h
@@ -0,0 +1,20 @@
+/**
+ * @file Offsets.h
+ * @author Asiern
+ * @brief 
+ * @version 0.1
+ * @date 2022-01-03
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#ifndef OFFSETS_H
+#define OFFSETS_H
+#include <Windows.h>
+
+typedef struct
+{
+} offsets;
+
+#endif;
